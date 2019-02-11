6f96b27ee5c9caa1cc0ac14c928e2d9a0fc2ffbd
redday059 <redday059@gmail.com>
2019-02-11 22:26:29 +0100

    ads users, fs, reads from file

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello world!')
-})
-
-app.listen(3000);
-
-// var fs = require('fs');
-// var _ = require('lodash');
-// var users = [];
-//
-// fs.readFile('users.json', {encoding: 'utf8'}, function (err, data) {
-//   if (err) throw err;
-//
-//   JSON.parse(data).forEach(function (user) {
-//     user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
-//     users.push(user)
-//   })
-//
-// });
-//
 // app.get('/', function (req, res) {
-//   var buffer = '';
-//
-//   users.forEach(function (user) {
-//     buffer += '<a href="/' + user.username + '">' + user.name.full + '</a><br>'
-//   });
-//   res.send(buffer)
+//   res.send('Hello world!')
 // });
-//
+
+var fs = require('fs');
+var _ = require('lodash');
+var users = [];
+
+fs.readFile('users.json', {encoding: 'utf8'}, function (err, data) {
+  if (err) throw err;
+
+  JSON.parse(data).forEach(function (user) {
+    user.name.full = _.startCase(user.name.first + ' ' + user.name.last);
+    users.push(user)
+  })
+
+});
+
+app.get('/', function (req, res) {
+  var buffer = '';
+
+  users.forEach(function (user) {
+    buffer += '<a href="/' + user.username + '">' + user.name.full + '</a><br>'
+  });
+  res.send(buffer)
+});
+
 // app.get(/big.*/, function (req, res, next) {
 //   console.log('BIG USER ACCESS');
 //   next();
@@ -44,7 +42,7 @@ app.listen(3000);
 //   var username = req.params.username;
 //   res.send(username);
 // });
-//
-// var server = app.listen(3000, function () {
-//   console.log('Server running at http://localhost:' + server.address().port)
-// });
+
+var server = app.listen(3000, function () {
+  console.log('Server running at http://localhost:' + server.address().port)
+});
